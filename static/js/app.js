df578a6fecb00c8caf563526706a9a5f57b64814
Alvin Lucero <alvinjaylucero1@gmail.com>
2021-07-01 18:13:02 +0800

    adding styles.css, updating index.html, finish app.js without bonus

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,38 @@
-// function plots(id){ 
-    // Read json
+// Function to initialise page plots
+function init() {
+    // Read json data
+    d3.json("../../data/samples.json").then((data) => {
+        
+        // Select drop down menu
+        var dropDownMenu = d3.select("#selDataset");
+
+        // Get id names to menu
+        data.names.forEach(function (name) {
+            dropDownMenu.append("option").text(name).property("value");
+        });
+        
+        // Call functions to plot graphs and subject info
+        plots(data.names[0]);
+        demographicInfo(data.names[0]);
+    });
+}
+
+
+// Function to create plots
+function plots(subjectId){ 
+    // Read json data
     d3.json("../../data/samples.json").then(data => {
-        // console.log(data);
+        console.log(data);
 
         // Grab values from the data json object to build the plots
         var sample_values = data.samples[0].sample_values.slice(0,10).reverse();
         var otu_ids = data.samples[0].otu_ids.slice(0,10).reverse();
         var otu_labels = data.samples[0].otu_labels.slice(0,10);
-        // console.log(otu_ids);
-
-
         var otu_id_label = otu_ids.map(x => "OTU " + x);
+        
+        
         // Create the trace for bar plot
-        var trace1 = {
+        var traceBar = {
             x: sample_values,
             y: otu_id_label,
             text: otu_labels,
@@ -20,15 +40,16 @@
             orientation: "h"
         };
         
-        // Create the data array for the plot
-        var data1 = [trace1];
+        // Create the data array for the bar plot
+        var dataBar = [traceBar];
         
         // Plot the chart to a div tag with id "bar"
-        Plotly.newPlot("bar", data1);
+        Plotly.newPlot("bar", dataBar);
         
         
+
         // Create trace for bubble plot
-        var trace2 = {
+        var traceBubble = {
             x: data.samples[0].otu_ids,
             y: data.samples[0].sample_values,
             mode: "markers",
@@ -40,17 +61,50 @@
         };
 
         // Create the data array for the plot
-        var data2 = [trace2];
+        var dataBubble = [traceBubble];
         
         // Create layout
-        var layout2 = {
+        var layoutBubble = {
             xaxis:{title: "OTU ID"}
         }
 
         // Plot the chart to a div tag with id "bubble"
-        Plotly.newPlot("bubble", data2, layout2);
+        Plotly.newPlot("bubble", dataBubble, layoutBubble);
     });
-// }
+}
 
-// https://waustralia.bootcampcontent.com/the-university-of-western-australia/waus-perth-virt-data-pt-03-2021-u-c/-/tree/master/02-Homework/15-Interactive-Visualizations-and-Dashboards/Instructions
-// https://github.com/v33na/Belly-Button-Biodiversity/blob/master/static/js/app.js
+
+// Function to get demographic information table
+function demographicInfo(subjectId) {
+    // read the json file to get data
+        d3.json("../../data/samples.json").then((data)=> {
+
+            // get the metadata info for the demographic panel
+            var metadata = data.metadata;
+    
+            // filter meta data info by id
+            var result = metadata.filter(meta => meta.id.toString() === subjectId)[0];
+            
+            // select demographic panel to put data
+            var demographicInfo = d3.select("#sample-metadata");
+            
+            // empty the demographic info panel each time before getting new id info
+            demographicInfo.html("");
+    
+            // grab the necessary demographic data data for the id and append the info to the panel
+            Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").append("strong").text(key[0].toLowerCase() + ": " + key[1] + "\n");    
+            });
+        });
+    }
+
+
+// Function to change plots
+function changePlot(subjectId){
+    plots(subjectId);
+    demographicInfo(subjectId);
+}
+
+
+// Initialise page
+init();
