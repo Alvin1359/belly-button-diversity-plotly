9babdd57e4fee72621f2c0e09bbcf0bb9a560e87
Alvin Lucero <alvinjaylucero1@gmail.com>
2021-07-01 23:35:11 +0800

    updating app.js updating plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,25 @@
-// Function to initialise page plots
-function init() {
-    // Read json data
-    d3.json("../../data/samples.json").then((data) => {
-        
-        // Select drop down menu
-        var dropDownMenu = d3.select("#selDataset");
-
-        // Get id names to menu
-        data.names.forEach(function (name) {
-            dropDownMenu.append("option").text(name).property("value");
-        });
-        
-        // Call functions to plot graphs and subject info
-        plots(data.names[0]);
-        demographicInfo(data.names[0]);
-    });
-}
-
-
 // Function to create plots
 function plots(subjectId){ 
     // Read json data
     d3.json("../../data/samples.json").then(data => {
-        console.log(data);
-
+        
+        // Filter data according to id
+        var filteredData = data.samples.filter(d => d.id == subjectId)
+        console.log(filteredData);
+        
         // Grab values from the data json object to build the plots
-        var sample_values = data.samples[0].sample_values.slice(0,10).reverse();
-        var otu_ids = data.samples[0].otu_ids.slice(0,10).reverse();
-        var otu_labels = data.samples[0].otu_labels.slice(0,10);
+        var sample_values = filteredData[0].sample_values
+        var otu_ids = filteredData[0].otu_ids
+        var otu_labels = filteredData[0].otu_labels
         var otu_id_label = otu_ids.map(x => "OTU " + x);
         
-        
+        // =============================
+        //          BAR CHART
+        // =============================
         // Create the trace for bar plot
         var traceBar = {
-            x: sample_values,
-            y: otu_id_label,
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_id_label.slice(0,10).reverse(),
             text: otu_labels,
             type: "bar",
             orientation: "h"
@@ -46,18 +31,20 @@ function plots(subjectId){
         // Plot the chart to a div tag with id "bar"
         Plotly.newPlot("bar", dataBar);
         
-        
+        // =============================
+        //          BUBBLE PLOT
+        // =============================
 
         // Create trace for bubble plot
         var traceBubble = {
-            x: data.samples[0].otu_ids,
-            y: data.samples[0].sample_values,
+            x: otu_ids,
+            y: sample_values,
             mode: "markers",
             marker: {
-                size: data.samples[0].sample_values,
-                color: data.samples[0].otu_ids
+                size: sample_values,
+                color: otu_ids
             },
-            text: data.samples[0].otu_labels
+            text: otu_labels
         };
 
         // Create the data array for the plot
@@ -65,46 +52,80 @@ function plots(subjectId){
         
         // Create layout
         var layoutBubble = {
-            xaxis:{title: "OTU ID"}
+            xaxis:{
+                title: "OTU ID"
+            }
         }
 
         // Plot the chart to a div tag with id "bubble"
         Plotly.newPlot("bubble", dataBubble, layoutBubble);
+
+        // =============================
+        //          GAUGE CHART
+        // =============================
+
+
     });
 }
 
 
 // Function to get demographic information table
 function demographicInfo(subjectId) {
-    // read the json file to get data
+    // Read the json file to get data
         d3.json("../../data/samples.json").then((data)=> {
 
-            // get the metadata info for the demographic panel
+            // Metadata info for the demographic info
             var metadata = data.metadata;
     
-            // filter meta data info by id
-            var result = metadata.filter(meta => meta.id.toString() === subjectId)[0];
+            // Filter info by subjectId
+            var result = metadata.filter(meta => meta.id.toString() == subjectId)[0];
             
-            // select demographic panel to put data
+            // Insert into sample-metadata
             var demographicInfo = d3.select("#sample-metadata");
             
-            // empty the demographic info panel each time before getting new id info
+            // Remove text from demographic info
             demographicInfo.html("");
     
-            // grab the necessary demographic data data for the id and append the info to the panel
+            // Append info to panel
             Object.entries(result).forEach((key) => {   
-                demographicInfo.append("h5").append("strong").text(key[0].toLowerCase() + ": " + key[1] + "\n");    
+                demographicInfo.append("h5").text(key[0].toLowerCase() + ": " + key[1] + "\n");    
             });
         });
     }
 
 
+// Function to initialise page plots
+function init() {
+    // Read json data
+    d3.json("../../data/samples.json").then((data) => {
+        
+        // Select drop down menu
+        var dropDownMenu = d3.select("#selDataset");
+
+        // Get subjectId from dropdown
+        data.names.forEach(function (name) {
+            dropDownMenu.append("option").text(name).property("value");
+        });
+        
+        // Call functions to plot graphs and show subject info
+        plots(data.names[0]);
+        demographicInfo(data.names[0]);
+    });
+}
+
+
 // Function to change plots
-function changePlot(subjectId){
+function optionChanged(){
+    // Select drop down menu
+    var dropdownMenu = d3.select("#selDataset");
+
+    // Assign the value of the dropdown menu option to a variable
+    var subjectId = parseInt(dropdownMenu.property("value"));
+
+    // Call back functions to update plots
     plots(subjectId);
     demographicInfo(subjectId);
 }
-
 
 // Initialise page
 init();
