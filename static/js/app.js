86f80a2e7f2ab29bf75ea62e955d3ae29029bb3f
Alvin Lucero <alvinjaylucero1@gmail.com>
2021-07-02 12:59:58 +0800

    adding app.js bonus, images

--- static/js/app.js
+++ static/js/app.js
@@ -4,14 +4,15 @@ function plots(subjectId){
     d3.json("samples.json").then(data => {
         
         // Filter data according to id
-        var filteredData = data.samples.filter(d => d.id == subjectId)
+        var filteredData = data.samples.filter(d => d.id == subjectId);
         console.log(filteredData);
         
         // Grab values from the data json object to build the plots
-        var sample_values = filteredData[0].sample_values
-        var otu_ids = filteredData[0].otu_ids
-        var otu_labels = filteredData[0].otu_labels
+        var sample_values = filteredData[0].sample_values;
+        var otu_ids = filteredData[0].otu_ids;
+        var otu_labels = filteredData[0].otu_labels;
         var otu_id_label = otu_ids.map(x => "OTU " + x);
+        var wfreq = data.metadata.filter(d => d.id == subjectId)[0].wfreq;
         
         // =============================
         //          BAR CHART
@@ -28,13 +29,18 @@ function plots(subjectId){
         // Create the data array for the bar plot
         var dataBar = [traceBar];
         
+        // Create layout
+        var layoutBar = {
+            height: 500,
+            width: 500
+            }
+        
         // Plot the chart to a div tag with id "bar"
-        Plotly.newPlot("bar", dataBar);
+        Plotly.newPlot("bar", dataBar, layoutBar);
         
         // =============================
         //          BUBBLE PLOT
         // =============================
-
         // Create trace for bubble plot
         var traceBubble = {
             x: otu_ids,
@@ -47,14 +53,16 @@ function plots(subjectId){
             text: otu_labels
         };
 
-        // Create the data array for the plot
+        // Create the data array for bubble chart
         var dataBubble = [traceBubble];
         
-        // Create layout
+        // Create layout for bubble chart
         var layoutBubble = {
             xaxis:{
                 title: "OTU ID"
-            }
+            },
+            height: 700,
+            width: 1200
         }
 
         // Plot the chart to a div tag with id "bubble"
@@ -63,9 +71,98 @@ function plots(subjectId){
         // =============================
         //          GAUGE CHART
         // =============================
+        // Scaling wfreq to 180 degree range
+        var level = wfreq * 20;
 
+        // Trig to calc needle point
+        var degrees = 180 - level, radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
 
+        // Path to create needle
+        var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+        var pathX = String(x);
+        var space = " ";
+        var pathY = String(y);
+        var pathEnd = " Z";
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        // Creating trace for gauge chart
+        var traceGauge = [
+            {
+              type: "scatter",
+              x: [0],
+              y: [0],
+              marker: { 
+                  size: 12, color: "850000" 
+                },
+              showlegend: false,
+              name: "Washes",
+              text: wfreq,
+              hoverinfo: "text+name"
+            },
+            {
+              values: [1, 1, 1, 1, 1, 1, 1, 1 ,1, 9],
+              rotation: 90,
+              text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              textinfo: "text",
+              textposition: "inside",
+              marker: {
+                colors: [
+                  "rgba(0, 105, 11, .5)",
+                  "rgba(10, 120, 22, .5)",
+                  "rgba(14, 127, 0, .5)",
+                  "rgba(110, 154, 22, .5)",
+                  "rgba(170, 202, 42, .5)",
+                  "rgba(202, 209, 95, .5)",
+                  "rgba(210, 206, 145, .5)",
+                  "rgba(232, 226, 202, .5)",
+                  "rgba(240, 230, 215, .5)",
+                  "rgba(255, 255, 255, 0)"
+                ]
+              },
+              labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+              hoverinfo: "label",
+              hole: 0.5,
+              type: "pie",
+              showlegend: false
+            }
+          ];
+        
+        // Creating layout for gauge chart
+        var layoutGauge = {
+        shapes: [
+            {
+            type: "path",
+            path: path,
+            fillcolor: "850000",
+            line: {
+                color: "850000"
+            }
+            }
+        ],
+        title: "<b>Belly Button Washing Frequency</b> <br>Scrubs per Week",
+        height: 700,
+        width: 700,
+        xaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        }
+        };
+        
+        // Plot gauge chart
+        Plotly.newPlot('gauge', traceGauge, layoutGauge); 
     });
+
 }
 
 
